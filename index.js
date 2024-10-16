0693b89ed4ff2c0a8741bf540f4047e847129d70
s1xte3n <ntnhlapo16@gmail.com>
2024-10-16 02:50:14 +0200

    4. Call Contract Functions(get_message)

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const {
     Hbar,
     FileCreateTransaction,
     ContractCreateTransaction,
+    ContractFunctionParameters,
+    ContractCallQuery,
 } = require("@hashgraph/sdk");
 
 require('dotenv').config();
@@ -74,6 +76,27 @@ async function environmentSetup() {
     //Log the smart contract ID
     console.log("The smart contract ID is " + newContractId);
 
-    //v2 JavaScript SDK
+    // Calls a function of the smart contract
+    const contractQuery = await new ContractCallQuery()
+        //Set the gas for the query
+        .setGas(100000)
+        //Set the contract ID to return the request for
+        .setContractId(newContractId)
+        //Set the contract function to call
+        .setFunction("get_message")
+        //Set the query payment for the node returning the request
+        //This value must cover the cost of the request otherwise will fail
+        .setQueryPayment(new Hbar(2));
+
+    //Submit to a Hedera network
+    const getMessage = await contractQuery.execute(client);
+
+    // Get a string from the result at index 0
+    const message = getMessage.getString(0);
+
+    //Log the message
+    console.log("The contract message: " + message);
+
+    //v2 Hedera JavaScript SDK
 }
 environmentSetup();
